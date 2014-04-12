e5d32d2a73f6e4a49f5190b860d11d71e9628fb2
RODragon <tchamber@mtu.edu>
2014-04-12 16:02:59 -0400

    Added Stylized Font to Assets
    Added the stylized font from Taylor's build to the assets.

--- src/Game.cpp
+++ src/Game.cpp
@@ -446,7 +446,9 @@ void Game::createClock(){
 	//device->setWindowCaption(tmp);
 	
 	//text that will be displayed on the screen
+	printf("loading font...\n");
 	text = smgr->addTextSceneNode(device->getGUIEnvironment()->getFont("../assets/bigfont.png"),tmp,video::SColor(255,0,0,0),0,core::vector3df(0,18,35));
+	printf("font loaded!\n");
 }
 
 /*
